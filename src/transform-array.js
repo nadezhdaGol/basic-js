a7a4b4b264e3a2d91d76714da8c8bd9a2f05b877
nadezhdaGol <78170166+nadezhdaGol@users.noreply.github.com>
2021-03-04 22:03:40 +0300

    feat: add solution of the task transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  
+  if (!Array.isArray(arr)) throw new Error();
+
+  let trArray = arr.map(function (value, index, array) {
+
+    if (value === '--double-prev') {
+      if (array[index - 2] === '--discard-next') {
+        return 'nothing';
+      } else {
+        return array[index - 1];
+      }
+    } else if (value === '--double-next') {
+      return array[index + 1];
+    } else if (array[index + 1] === '--discard-prev') {
+      return 'nothing';
+    } else if (value === '--discard-prev') {
+      return 'nothing';
+    } else if (array[index - 1] === '--discard-next') { 
+      return 'nothing';
+    } else if (value === '--discard-next') {
+      return 'nothing';
+    } else {
+      return array[index];
+    }
+    
+  });
+
+  let filteredArray = trArray.filter(val => val !== 'nothing' && val !== undefined);
+
+  return filteredArray;
+
 };
